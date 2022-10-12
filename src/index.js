f22ebc943dc2fa6e652cb34cdd33a927e74716c2
https://github.com/AdityaKuntal/backen-nodejs.git <akjaat001kuntal@gmail.com>
2022-10-12 22:36:49 +0530

    updatenewchanges

--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+
+
+
+// const value ={
+//     usre:"aditya ",
+//     pass :"pass"
+// }
+// const {a,b} = value
+
+// // object2.usre="xyr"
+// console.log(a)
+// console.log(object2)
+// object2.usre="xyr"
+ 
