e062bb49e6fb69c868d03a968f3b65d1ada3b755
https://github.com/AdityaKuntal/backen-nodejs.git <akjaat001kuntal@gmail.com>
2022-10-31 22:33:18 +0530

    update chnages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyparser = require ('body-parser')
 const route = require('./route/route')
 const app = express();
 const mongoose = require('mongoose')
-
+const moment = require('moment')
 app.use(bodyparser.json())
 
  mongoose.connect("mongodb+srv://Adityakunta:IPSyBcf7uKmQrNcH@aditya.4payvyl.mongodb.net/?retryWrites=true&w=majority",{
@@ -12,7 +12,19 @@ app.use(bodyparser.json())
 
 .then(()=>console.log("Hukum mere aakaa  🪁  batao kya krna ab mujko  😄   "))
 .catch(err=> console.log(err))
-app.use('/',route)
+
+app.use((req, res, next) => {
+    let ip=req.ip;
+    let path=req.originalUrl;
+    let timeStamp=moment().format("YYYY-MM-DD hh:mm:ss");
+
+    console.log(timeStamp, ip,path)
+    next();
+  })
+
+app.use('/', route);
+
+
 
 app.listen(process.env.Port||3000,function(){
     console.log("Aupn Chalne Ko Tyar Hai "+ (process.env.Port||3000+  " Mongoose Tum Ko Tumhra Maalik Yaad Kar Rha Hai   😃  "))
--- src/route/route.js
+++ src/route/route.js
@@ -11,6 +11,25 @@ Router.get("/myUpdate",bookController.myUpdate)
 Router.put("/myGetBooks",bookController.myGetBooks)
 
 
+const moment = require('moment');
+const { path } = require('express/lib/application');
+Router.get("/dateManipulations", function (req, res) {
+    
+    // const today = moment();
+    // let x= today.add(10, "days")
+
+    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
+    // console.log(validOrNot)
+    
+    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+
+    let x= dateB.diff(dateA, "days")
+    console.log(x)
+
+    res.send({ msg: "all good"})
+})
+
 
 
 
